8d22da114e5a70cceb550d5f5a5592cbb1c91d7f
Abdi9739 <103022360+Abdi9739@users.noreply.github.com>
2022-04-11 16:11:17 +0300

    Add files via upload

---
+++ volume sphere cat.c
@@ -0,0 +1,18 @@
+//a program to calculate volume of a sphere
+
+#include <stdio.h>
+
+int main()
+
+{
+
+int r;
+
+float pie=3.14285714286;
+
+double volume=(4.0/3.0)*pie*(r*r*r);
+
+printf("Volume of the sphere=%f",volume);
+
+}
+
